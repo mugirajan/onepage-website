584dcdfbe2f7aa05512d77db183b2a07c99feabf
mugilavathi <mugirajan95@gmail.com>
2023-03-30 11:46:24 +0530

    add my file

---
+++ main.js
@@ -0,0 +1,57 @@
+var button=document.getElementById("burger-icon");
+var div=document.querySelector(".menu-bar");
+var link1=document.querySelector(".link1");
+var link2=document.querySelector(".link2");
+var link3=document.querySelector(".link3");
+var link4=document.querySelector(".link4");
+var link5=document.querySelector(".link5");
+var link6=document.querySelector(".link6");
+var link7=document.querySelector(".link7");
+var link8=document.querySelector(".link8");
+
+button.addEventListener("click",()=>{
+    div.style.display="flex";
+});
+
+function clear(){
+    div.style.display="none";
+}
+
+link1.addEventListener("click",()=>{
+    clear()
+});
+
+link2.addEventListener("click",()=>{
+    clear()
+});
+
+link3.addEventListener("click",()=>{
+    clear()
+});
+
+link4.addEventListener("click",()=>{
+    clear()
+});
+
+link5.addEventListener("click",()=>{
+    clear()
+});
+
+link6.addEventListener("click",()=>{
+    clear()
+});
+
+link7.addEventListener("click",()=>{
+    clear()
+});
+
+link8.addEventListener("click",()=>{
+    clear()
+});
+
+
+
+
+
+
+ 
